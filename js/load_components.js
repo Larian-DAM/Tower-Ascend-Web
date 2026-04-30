73508ab9db72f7cac5d4dac740f12603d8c37a4f
Larian <larian.gsdam@gmail.com>
2026-04-30 12:06:19 +0100

    Add files via upload

---
+++ js/load_components.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function loadComponents(id, file) {
+    fetch(file).then(res => res.text()).then(data => {
+        document.getElementById(id).innerHTML = data;
+    })
+}
+
+loadComponents("header", "./components/header.html");
+loadComponents("footer", "./components/footer.html");
